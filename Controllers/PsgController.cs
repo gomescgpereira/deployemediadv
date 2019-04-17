17f03c8cd8906e890889d222c0b0f4067dc85eab
gomescgcpereira <gomescgconsultoria@gmail.com>
2019-04-17 09:08:16 -0300

    Deploy Google Nuvem

--- Controllers/PsgController.cs
+++ Controllers/PsgController.cs
@@ -6,9 +6,13 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using devmedia.Dominio;
 using devmedia.Dominio.interfaces;
+using Swashbuckle.AspNetCore.Swagger;
+
+
 
 namespace devmedia.Controllers
 {
+    [Produces("application/json")]
     [Route("api/[controller]")]
     [ApiController]
     public class PsgController : ControllerBase
@@ -22,13 +26,18 @@ namespace devmedia.Controllers
 
         // GET: api/Psg
         [HttpGet]
+        [ProducesResponseType(201)]
+        [ProducesResponseType(400)] 
         public IEnumerable<Pessoa> Get()
         {
             return _repository.GetList();
         }
 
         // GET: api/Psg/5
+
         [HttpGet("{id}", Name = "Get")]
+        [ProducesResponseType(200)]
+        [ProducesResponseType(400)] 
         public Pessoa Get(int id)
         {
             return _repository.GetPessoaById(id);
@@ -36,6 +45,8 @@ namespace devmedia.Controllers
 
         // POST: api/Psg
         [HttpPost]
+        [ProducesResponseType(201)]
+        [ProducesResponseType(400)] 
         public void Post([FromBody] Pessoa value)
         {
             _repository.InsertPessoa(value);
@@ -43,6 +54,9 @@ namespace devmedia.Controllers
 
         // PUT: api/Psg/5
         [HttpPut("{id}")]
+        [ProducesResponseType(202)]
+        [ProducesResponseType(404)] 
+        [ProducesResponseType(400)] 
         public void Put(int id, [FromBody] Pessoa value)
         {
             _repository.UpdatePessoa(value );
@@ -51,6 +65,9 @@ namespace devmedia.Controllers
         
         // DELETE: api/ApiWithActions/5
         [HttpDelete("{id}")]
+        [ProducesResponseType(200)]
+        [ProducesResponseType(400)] 
+        [ProducesResponseType(404)] 
         public void Delete(int id)
         {
             _repository.Delete(id);
--- Startup.cs
+++ Startup.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+//using System.IO;
+//using System.Reflection;
 using System.Threading.Tasks;
 using devmedia.repositorio;
 using devmedia.Dominio.interfaces;
@@ -13,6 +15,8 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.HttpOverrides;
+using Swashbuckle.AspNetCore.Swagger;
 
 namespace devmedia
 {
@@ -30,8 +34,17 @@ namespace devmedia
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
 			services.AddCors();
-          //  services.AddTransient<IRepositoriesPessoa, RepositoriesPessoa>();
-            services.AddTransient<IRepositoriesPessoaPsg, RepositoriesPessoaPsq>();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info {  Title = "My APi", Version = "v1" });
+               // var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+               // var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+               // c.IncludeXmlComments(xmlPath);   
+            });
+
+            
+                //  services.AddTransient<IRepositoriesPessoa, RepositoriesPessoa>();
+              services.AddTransient<IRepositoriesPessoaPsg, RepositoriesPessoaPsq>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -47,12 +60,27 @@ namespace devmedia
             }
 
             app.UseHttpsRedirection();
+            
+            app.UseForwardedHeaders(new ForwardedHeadersOptions
+            {
+                ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto
+            });
+
+           
+            // app.UseAuthentication();
+
 
 			app.UseCors(builder => builder
 				.AllowAnyOrigin()
 				.AllowAnyMethod()
 				.AllowAnyHeader()
 				.AllowCredentials());   
+             app.UseSwagger();
+             app.UseSwaggerUI(c => {
+              
+               c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API Demostração");
+               c.RoutePrefix = "api/v1/swagger";
+             });
 
             app.UseMvc();
             
